491aaed49d9f793877c6f8dd807cccb9ad55c345
Jason Bellamy <j@sonbellamy.com>
2016-07-23 19:17:06 -0400

    Add an is-lower-case function

---
+++ src/util/is-lower-case.js
@@ -0,0 +1,11 @@
+/**
+ * Check if the the provided ascii value is lower case
+ *
+ * @name isLowerCase
+ * @function
+ * @param {Number} x the unicode number
+ * @returns {Boolean} is the provided value lower case
+ */
+export function isLowerCase(x) {
+  return (x >= 97 && x <= 123);
+}
---
+++ test/util/is-lower-case.spec.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+import { range } from '../../src/util/range.js';
+import { isLowerCase } from '../../src/util/is-lower-case.js';
+
+
+test('checks if the provided ascii value is lower case', (t) => {
+  t.equals(
+    range(97, 123).every(isLowerCase),
+    true
+  );
+
+  t.notEquals(
+    range(65, 90).every(isLowerCase),
+    true
+  );
+
+  t.end();
+});
