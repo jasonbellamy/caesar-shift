399d120300758404561c4c1c40295176ca699810
Jason Bellamy <j@sonbellamy.com>
2016-07-23 12:49:23 -0400

    Add encrypt/decrypt function stubs

---
+++ src/caesar-shift.js
@@ -0,0 +1,25 @@
+/**
+ * Encrypt a message using the Caesar cipher
+ *
+ * @name encrypt
+ * @function
+ * @param {Number} key the number of positions to shift each character in the message by
+ * @param {String} message the message to encrypt
+ * @returns {String} the encrypted message
+ */
+export function encrypt(key, message) {
+
+}
+
+/**
+ * Decrypt a message thats been encrypted using the Caesar cipher
+ *
+ * @name decrypt
+ * @function
+ * @param {Number} key the key used to decrypt the message
+ * @param {String} message the message to decrypt
+ * @returns {String} the decrypted message
+ */
+export function decrypt(key, message) {
+
+}
